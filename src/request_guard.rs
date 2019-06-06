03a66def57f38ebcf8e76f20f3cb982f6384479a
wendellliu <cuk.bas@gmail.com>
2019-06-06 17:28:20 +0800

    handle ping event well

--- src/request_guard.rs
+++ src/request_guard.rs
@@ -11,20 +11,22 @@ pub struct WebhookSecret(pub String);
 
 #[derive(Debug)]
 pub enum WebhookError {
-  Missing
+  Missing,
+  NotAllow
 }
 
 impl<'a, 'r> FromRequest<'a, 'r> for WebhookSecret {
   type Error = WebhookError;
   fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-    match request.headers().get_one(X_GITHUB_EVENT) {
-      Some("ping") => return Outcome::Forward(()),
-      _ => ()
+    let secret = match request.headers().get_one(HUB_SIGNATURE) {
+      Some(s) => s,
+      None => return Outcome::Failure((Status::BadRequest, WebhookError::Missing))
     };
 
-    match request.headers().get_one(HUB_SIGNATURE) {
-      Some(s) => Outcome::Success(WebhookSecret(s.to_string())),
-      None => Outcome::Failure((Status::BadRequest, WebhookError::Missing))
+    match request.headers().get_one(X_GITHUB_EVENT) {
+      Some("ping") => Outcome::Forward(()),
+      Some("delete") => Outcome::Success(WebhookSecret(secret.to_string())),
+      _ => Outcome::Failure((Status::BadRequest, WebhookError::NotAllow))
     }
   }
 }
